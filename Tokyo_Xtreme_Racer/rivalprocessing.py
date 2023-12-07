a87daf4db42126328c0b5d51e8b4f00d84e2d41a
Kinglink <Frank.Reese@gmail.com>
2023-12-07 01:21:50 -0800

    Added new feast achievement. Added stored Max health code notes for Feast Achievement. Commented out They Can’t Handle Any More Power as it's in the middle of being replaced Fixed a capitalized M on True Legend of the Three Kingdoms

---
+++ Tokyo_Xtreme_Racer/rivalprocessing.py
@@ -0,0 +1,542 @@
+# Define the input data
+data = """
+Rolling Guy
+Rolling Guy 1 (Rolling Bastard 1)
+Rolling Guy 2 (Rolling Bastard 2)
+Rolling Guy 3 (Rolling Bastard 3)
+Rolling Guy 4 (Rolling Bastard 4)
+Rolling Guy 5 (Rolling Bastard 5)
+Rolling Guy 6 (Rolling Bastard 6)
+
+Little Gang
+Happy Run (Happy and Run)
+Kiss My Bumper (Kiss My Bumper)
+Talking Head (Talking Headbutt)
+Mr. Thanks (Mister Thank You)
+Spring Bird
+Lazy Butt (Lazy Tarō)
+
+SS Limited
+Old Star (Founder First Star)
+Heaven's Door (Heaven's 4-Door)
+Black Rain
+Red Thunder (Shutokō Red Flash)
+Sports Killer (Sports Car Killer)
+New Star (Originator First Star)
+Chemistry (Chemical Itō)
+Excite Carmine
+
+Knife and Forks
+Invincible Dinner (Matchless Full Course)
+Table Manner
+Street Wine (Highway Romanée-Conti)
+Golden Cream Soup (Golden Cream Sauce)
+Midnight à la Mode
+4 Star Driver
+Red Table Cloth (Crimson Tablecloth)
+
+Black Knights
+Sword of Dragon
+Dark Varlet (Dark Attendant)
+Dark Killer (Dark Executioner)
+Dark Eyes
+Dark Crow (Shibuya Crow)
+Dark Cloud (Shrouding Dark Cloud)
+Dark Miracle (Black Clothed Miracle)
+
+Fine Drive
+Red Meteor (Crimson Meteor)
+Sylvester (Silvester)
+Hell's Drifter
+King of 2000cc (Shimokitazawa 2000cc)
+Drift King (Drift Baby)
+Funky Monkey
+Natural Soldier
+Fire Bird
+
+RATT
+Flying Sasuke (Monkey Flight Sasuke)
+Red Shadow
+Thunder Bird (Great Runner Mako)
+Rental Driver
+Gamer Racer
+Drift Queen
+
+Queen's Paradise
+Shocking Joe
+Sunday Driver (Sunday Kōji)
+Balalaika Man (Balalaika Katō)
+Chopper (Chopper Tanaka)
+Lucky Guy (Lucky Man)
+Ballad (Dawn Ballade)
+Mr. Mystery (Phantom of Lohengrin)
+
+RINGS
+Lonely Jackal (Solitary Jackal)
+Shocking Slide Tail (Vivid Slide Tail)
+Meteor (Meteor Yōsuke)
+Tune Up Junky (Tune Shop Hamada)
+Silver Angel
+White Devil (Sonic White Devil)
+Flume (Carnage)
+Myna Bird (Hill Myna)
+
+Curving Edge
+Highway Prince (Shutokō Prince)
+Mr. Fire (Scorching Kōji)
+Wind of Night (25-Hour Gale)
+Professor Robledo (Professor Taka)
+Thunder Flare
+Road Killer (Straight Line Demon)
+
+Galaxy Racers
+Mr. Firm (Dependable Club President)
+Broken Arrow
+Lovely Erotica (Love and Fantasy Erotica)
+Mr. Othello
+Sweet Honey
+Sabrina (Kyōbashi Sabrina)
+
+Max Racing
+Bloody Rose
+Golden Beast
+Crazy Hooligan
+Blue Shadow
+Dark Panther (Death Black Panther)
+High Speed Blue Thunder
+High Speed White Blade
+High Speed Blood Rain
+
+Elegant Wild
+300 Mile Red Ruby
+Deep Green
+White Revolution
+Hostman Hut (Hostman Hat)
+Black Cinderella
+Unfinish Yellow (Infini Yellow)
+
+Departures
+Happy Chappy
+Night Killer (Midnight Executioner)
+Sweet Surrender
+Cool Sight (Cool Site)
+Great Finance
+Up Beat
+Crystal Prince (Glass Nobleman)
+
+Twister
+Burning Mirage (Scorching Mirage)
+Smile (Smile 0 Yen)
+Sweet Blues
+Blue Writer
+Shooting Star
+Baron (Baron Kagetsu)
+Rainy Day (Shutokō Drizzle)
+
+Catz
+Fire Revenge (Flame Revenger)
+Dark Mist (Dark Mantle)
+Highway Wall (Shutokō Wall)
+Wine Chase
+Firefighter (Firefighter Masumi)
+Fire Driver (Flame Sedan Driver)
+Unlucky Duke
+
+The Road of Justice
+Hearing Master
+Highway Magician (Shutokō Magician)
+Fighting Master (Fighting Nemoto)
+Wall Street (Wall Man)
+Flame Canon (Flame Cannon)
+Shining
+Happy Camper (Happy Rabbit)
+
+Diamond Image
+Thunder Shift (Lightning Shift Takuya)
+Lone Wolf
+Spring Rain (Spring Roll Itsuki)
+Silver Bullet
+Troubleshooter (Kanjō Airblade)
+Yellow Angel
+
+Rhythm Box
+Hip Hop Red
+Jazz Green
+Classical Blue
+Stormy Calligrapher (Raging Choreographer)
+Pink Groove
+Midnight Blue
+
+Wind Stars
+Requiem (Wolf's Requiem)
+R Magic
+Diamond Dust
+Yellow Flare
+Mad Train (Wangan Runaway Locomotive)
+Speedy Valkyrie (Swift-Footed Valkyrie)
+Blue Star (Blue Giant Star)
+Moody Queen
+
+Harmonize
+Visual Driver
+Curve Block (Corner Block Gojō)
+Highway Silver Arrow (Shutokō Silver Arrow)
+Aero Star
+Water Drift (Water Drift Ashiya)
+Brand New Thunder
+Dream Light (Dreaming Flash)
+
+Super Speed Wagon
+Revolution (Revolutionary Flag Bearer)
+Magical
+Belly-ache (Whiney Tarō)
+Fruit Basket (Fruity Bucket)
+Jumping (Splash Jirō)
+Shuffle
+
+Another Star
+Blue Speed
+Silver Speed
+Lightning Bird (Razor Miura)
+Red Shut (Red Shout)
+Battle Scratch
+Lightning Eyes (Thunder Hitomi)
+U Violet (Electromagnetic Trip)
+
+Gesellschaft
+Time is Money
+Lost Kid (Dawn Lost Grass)
+Lone Survivor (Lonely Survivor)
+Cool Guy (Cool Advertiser)
+Happy Extreme
+Angry Princess
+
+Explosion
+Wind Blade (Scythe Weasel)
+Golden Fang (Gold Fang)
+Trouble Maker
+Silent Sense
+Blue Phoenix
+Through Dog
+
+Highway Outlaw
+Spirit Crasher (Spirit Crusher)
+Pride Killer
+Cold Prince (Coldhearted Emotionless Nobleman)
+Mad Driver
+Driving Sir
+Totem Pole
+Spiral
+
+R.Gangs
+Drift Dancer (Shiver Drift Dancer)
+Street Dancer
+Hell's Bartender
+Professor Speedy (Speed Professor)
+Wind Impala
+Black Mirage
+Rain Drop
+
+Cupid Arrows
+Sonic Runner
+Mysterious Nymph (Dazzling White Fairy)
+Moon Light Child
+Cowboy Girl
+Silver Impact
+246 Heart Breaker
+
+Tokio Jungle
+Ion (Io)
+MJ6feet6
+Black Wind (Jet Hot Black Wind)
+White Fox
+Salty Dog
+Slide Freek (Slide Freak)
+Driving Blue Wave (Running Blue Wave)
+
+TR Racing
+Unstoppable Dream (Unstoppable Black Dragon's Dream)
+Lovely Girl (Lovely♪Rina)
+Rule Breaker (Rule Breaker Shinji)
+180 Master
+Dawn Shaker
+Night Worker (Night Worker Horī)
+Dancing Chaser
+
+Top Level
+Car Maniac (Tarmac Suzuki)
+Mystery Girl (Phantom Beautiful Girl)
+Red Tears (Tragic Crimson Tears)
+Sir General (Strategist Crimson)
+Legendary Digger (Legendary Worker)
+Midnight Through
+Russian Blue (Russian Blue Akane)
+
+Free Way
+Blue Meteor (Blue Comet)
+Orange Head
+Silver Fox
+Nightmare (Wangan Nightmare)
+Silent Reviver
+Ultimate Killer
+Crystal Knight
+Speed Junky (Accel Junky)
+Highway Bomber
+
+Dry Cruise
+Tobby (Scaffolding Masa)
+Midnight Owl (Midnight Hawks)
+Entrepreneur (Humiliation Contractor)
+Rain Master
+Mr. Max
+Mr. Muscle
+Angel Smile
+
+E.R.O.
+Mr. Terminate (Partitioner Sudō)
+Non Stop Emotion
+Brand New Morning
+Cool B
+Star Holder (Star Catching Man)
+Sentimental Flame
+Strange Devil
+
+Team Alpha
+Texas' White Wolf
+Gattling Gun (Gattling Joe)
+Mike the Horseman
+Smiling Shadow (Mocking Shadow)
+Rock Spider
+Code F
+
+True Ride
+A+ Driver (Perfect Score Driver)
+Large Impact (Giant Impact)
+Iron Blocker
+Genius Rookie
+Wolter Wolf (Walter Wolf)
+Pink Lily
+Strike Star
+Sunny Dragon
+
+Unlimited
+Killing Machine
+Lone MONSTER (Wandering MONSTER)
+Scotch (Scotch Limited Express)
+Yesman (Yes-Man Nao)
+Daimajin (Yokohane Great Warlock)
+Spider (Pit Viper Kyō)
+Sleepy Driver (Sleeping Kei)
+
+R.R.
+Silver Prince (Silvery Nobleman)
+Turning Artist (Cornering Artist)
+Asphalt's Cry (Asphalt's Sorrow)
+HIROSHI
+Dark Memory (Dark Memorial)
+Risky Cat
+Black Bird (Dark Ominous Bird)
+High Speed Star
+
+Thunder Dragoon
+Black Dragon
+White Dragon
+Yellow Dragon
+Red Dragon
+Green Dragon
+Gray Dragon
+Blue Dragon
+
+Abflug SRT
+Abf911
+Dandy
+Superman
+Teru
+Horse Head (Forehead Flick)
+
+Commander
+Speed Runner (Senna Foot Ginji)
+Mr. Japan (Yamato Saburō)
+Shoe Keeper (Two-Footed Hide)
+Weak Arm (Gentle Arm Sakae)
+Girl Hunter (Seducer Shū)
+Pickle (Ice Axe Noboru)
+Rainbow Traveler (Rainbow-Colored Traveler)
+Swimmer (Swimmer Hayato)
+
+Double Mind
+Yellow Voltage (Yellow Lightning Flash)
+Dark Labyrinth (Brown Satan)
+Blood (Bloody Cage)
+Gray Giant (Red Giant)
+Brown Giant (Brown Devil)
+White Angel (Pale Angel)
+Gray Fairy (Gray Spirit)
+
+APS
+Jamz (Yokohane Heavy Fighter Jet)
+Blocky Rider (Box-Riding Masahiro)
+King (Harima Emperor)
+Revived Wizard (Reviving Wizard)
+Shyxgirl (Rotary Flower)
+Double Booking (Hotel Over Rev)
+New Wind (Nür Wind)
+
+Fanatic Future
+Burning Leader (Burning Nakamura)
+Cool Looking (Cool Hosoda)
+Mysterious Driver (Mysterious Matsuzawa)
+Burning General (Burning Warlord)
+Intelligent Teacher
+Larger than Life (Extraordinary Man)
+Love Knuckle
+
+Speed Master
+Iceman
+Crazy Moon (Full Moon Insanity)
+Visual Tail
+Sunset Rider (Sunset Liner)
+White Witch
+Pearl Kiss (Pearl White Kiss)
+Maniac Player
+Black Angel
+Unknown Hero (Nameless Hero)
+Fast Beat
+
+Wanderers
+Lightning (Kamikaze Koji)
+White Panther (Obsessed White Panther)
+Garage Door (Garage Haruo)
+Speed Drinker (Speed Drunker)
+Lonely Wolf
+Steam Storm (Scorching Storm)
+Invincible Artist (Immortal Clown)
+Lost Freak (Straying Idol Freak)
+Bloody Mary
+Illusion B
+Caffeine Addict (Caffeine X3)
+Magazine Freak (Porno Suzuki)
+Machine of Inferno
+Anguish Barbarian (Anguished Barbarian)
+Waist Master (Waste Hirohito)
+Master Position
+Pretty Affair (Shonan Affair)
+Devil Driver (Death God Driver)
+Street Gambler (Street Murder Gambler)
+Green Wild (Green Wild Child)
+Dark Desperado (Jet Black Desperado)
+Eternity (Balsam Eternity)
+Mosquito Lemon
+Fearful Princess (Impatient Speed Princess)
+Dumb yet Smart (Ignorant Knowledge)
+Wild Heart
+Road Master
+Glamour Girl (Glamorous Mami)
+God Year (God Ear)
+Raspberry Kiss
+Craft Man (Craftsman Manabe)
+Cutie Pie (Cutie Hip)
+Storm Screamer (Storm Streamer)
+Uncrushable Alexander (Dauntless Alexander)
+Silver Ruler (Steel Triangular Ruler)
+Night Wolf (Midnight Silver Wolf)
+Max Energy (Limit Elegy)
+Jet Skater
+Crying Baby (Cry Baby)
+Tomorrow Again
+Healthy Hero (Healthy Hiro)
+Late Night Cinderella (After Midnight Cinderella)
+Unbalanced Heart (Unbalance Ishii)
+Toll Free (Free Tree)
+Madness (Mad Fat)
+Technician Guy (Technician Ono)
+Fast Phobe (Stalking Hammer)
+ZERO
+SPEED BOX
+Mask (Masked Étranger)
+Windy Fanfare (Dash Fanfare)
+Start Dash (Start Dash Maki)
+Golden License (Gold License)
+Iron Racer
+Exotic Butterfly
+King of Eden
+Loyal Knight
+Gentle Rain
+Last Flight
+Silent Barbarian
+
+13 Devils
+Traitor Jack knife (Betrayal Jack Knife) Traitor Jack Knife
+True Slide True Slide
+Unhappy Angel (Gloomy Angel) Dejected angel
+Hard Weapon Hard Weapon
+Fallen Blade (Depraved Blade)Fallen Blade
+Blood Hound Blood Hound
+Dark Producer (Lucifer Ōtsuka) Dark Producer
+Mr. Sitar (Sitar Kaneyama) Mr. Sitar
+Iron Heart (Steel Heart) Iron Heart 
+Dying Star Dying Star-0
+Dreamy Ghost (Dreaming Wraith)Dreamy Ghost
+Tail Gunner (Pursuit Tail-gunner) Tail Gunner
+Shadow Eyes Shadow eyes icon
+Speed King - [S1] (Jintei, Emperor Speed) Tim2out571571
+
+The Zodiac
+Midnight Rose Midnight rose icon
+Golden Wind (Golden Gale) Golden wind icon
+Dark Ringleader (Shadow Rebel) Dark ringleader icon
+Hard Lift (Hard Riff) Hard lift icon
+Blue Pressure Blue pressure icon
+No-Crown King (Uncrowned Emperor) Nocrown king icon
+Crying Pluto (Grief Pluto) Crying pluto icon
+Red Devil (Crimson Devil) Red devil icon
+Queen's Knight Queens knight icon
+Purple Meteor Purple meteor icon
+Exotic Eve (Exhaust Eve) Exotic eve
+White Charisma [S2] (White Phantom) White charisma icon
+
+Final Boss
+Unknown - [U] (???) Devilz icon
+"""
+
+# Split the data into lines and initialize variables
+lines = data.strip().split('\n')
+result = []
+group_name = ""
+line_counter = 0
+value = 1
+
+# Process each line
+for line in lines:
+    print (line)
+    # Remove text within parentheses using regex
+    import re
+    line = re.sub(r'\([^)]*\)', '', line).strip()
+    
+    if not line:
+        # Set line counter to 0 for empty lines or lines with only white space
+        line_counter = -1
+    elif line_counter == 0:
+        # Read the line as the group name and remove it from the results
+        group_name = line
+    else:
+        # Append the group name based on the line counter
+        if line_counter == 1:
+            result.append(f"{value}={line} ({group_name} Leader)")
+        else:
+            result.append(f"{value}={line} ({group_name} {line_counter - 1})")
+        value += 1
+    line_counter += 1
+    
+# Join the processed lines into a single string
+processed_data = '\n'.join(result)
+
+# Specify the output file name
+output_file = "numbered_rivals.txt"
+
+# Write the processed data to the output file
+with open(output_file, "w") as file:
+    file.write(processed_data)
+
+# Print the processed data
+print(processed_data)
